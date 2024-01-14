7e647ac5681d52b4b373ca262ad661966cd8161f
Andrey <69705895+SheffAndrey@users.noreply.github.com>
2024-01-15 00:10:41 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function hello(){
+    // document.getElementById("demo").innerHTML = 'Остановить';
+    //alert("Привет Мир!");   // Выводит диалоговое окно с надписью "Привет Мир!"
+    // const a = 158;
+    // const b = 2;
+    // const result=a + b;
+    // alert(result);
+    
+    // let num1 = parseInt(prompt("Введите первое число"));
+    // let num2 = parseInt(prompt("Введите втрое число"));
+    // alert(`${num1} + ${num2} = ${num1 + num2}`);
+
+    let name = prompt("Введите Ваше имя: ");
+    alert("Привет " + name);
+    
+}
+
+
+
+
+
